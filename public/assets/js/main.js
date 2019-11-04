24c6d3fa3c769f4b26a6af6e207b7a86e0420513
Holly Davis <hollydavis541@gmail.com>
2019-11-03 19:21:29 -0800

    carousel partially working

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -1,3 +1,31 @@
+// var images = [
+//   'images/brokentop.jpg',
+//   'images/hollyface.jpg',
+//   'images/cookies.png'
+// ]
+
+// var imageUrl = 'images/brokentop.jpg';
+
+// // https://stackoverflow.com/questions/512054/setting-background-image-using-jquery-css-property
+
+// $('#main>section.slideshow').css( 'background-image', 'url(' + imageUrl + ')');
+
+// // https://stackoverflow.com/questions/37242754/how-do-i-change-the-background-image-every-x-seconds/37243062
+
+
+
+
+// var i = 0;
+// setInterval(function() {
+//   imageHead.style.backgroundImage = 'url(' + images[i] + ')';
+//   i = i + 1;
+//   if (i == images.length) {
+//     i = 0;
+//   }
+// }, 3000);
+
+
+
 /*
 	Prologue by HTML5 UP
 	html5up.net | @ajlkn
@@ -6,118 +34,118 @@
 
 (function($) {
 
-	var	$window = $(window),
-		$body = $('body'),
-		$nav = $('#nav');
+  var	$window = $(window),
+    $body = $('body'),
+    $nav = $('#nav');
 
-	// Breakpoints.
-		breakpoints({
-			wide:      [ '961px',  '1880px' ],
-			normal:    [ '961px',  '1620px' ],
-			narrow:    [ '961px',  '1320px' ],
-			narrower:  [ '737px',  '960px'  ],
-			mobile:    [ null,     '736px'  ]
-		});
+  // Breakpoints.
+  breakpoints({
+    wide:      [ '961px', '1880px' ],
+    normal:    [ '961px', '1620px' ],
+    narrow:    [ '961px', '1320px' ],
+    narrower:  [ '737px', '960px' ],
+    mobile:    [ null, '736px' ]
+  });
 
-	// Play initial animations on page load.
-		$window.on('load', function() {
-			window.setTimeout(function() {
-				$body.removeClass('is-preload');
-			}, 100);
-		});
+  // Play initial animations on page load.
+  $window.on('load', function() {
+    window.setTimeout(function() {
+      $body.removeClass('is-preload');
+    }, 100);
+  });
 
-	// Nav.
-		var $nav_a = $nav.find('a');
+  // Nav.
+  var $nav_a = $nav.find('a');
 
-		$nav_a
-			.addClass('scrolly')
-			.on('click', function(e) {
+  $nav_a
+    .addClass('scrolly')
+    .on('click', function(e) {
 
-				var $this = $(this);
+      var $this = $(this);
 
-				// External link? Bail.
-					if ($this.attr('href').charAt(0) != '#')
-						return;
+      // External link? Bail.
+      if ($this.attr('href').charAt(0) != '#')
+        return;
 
-				// Prevent default.
-					e.preventDefault();
+      // Prevent default.
+      e.preventDefault();
 
-				// Deactivate all links.
-					$nav_a.removeClass('active');
+      // Deactivate all links.
+      $nav_a.removeClass('active');
 
-				// Activate link *and* lock it (so Scrollex doesn't try to activate other links as we're scrolling to this one's section).
-					$this
-						.addClass('active')
-						.addClass('active-locked');
+      // Activate link *and* lock it (so Scrollex doesn't try to activate other links as we're scrolling to this one's section).
+      $this
+        .addClass('active')
+        .addClass('active-locked');
 
-			})
-			.each(function() {
+    })
+    .each(function() {
 
-				var	$this = $(this),
-					id = $this.attr('href'),
-					$section = $(id);
+      var	$this = $(this),
+        id = $this.attr('href'),
+        $section = $(id);
 
-				// No section for this link? Bail.
-					if ($section.length < 1)
-						return;
+      // No section for this link? Bail.
+      if ($section.length < 1)
+        return;
 
-				// Scrollex.
-					$section.scrollex({
-						mode: 'middle',
-						top: '-10vh',
-						bottom: '-10vh',
-						initialize: function() {
+      // Scrollex.
+      $section.scrollex({
+        mode: 'middle',
+        top: '-10vh',
+        bottom: '-10vh',
+        initialize: function() {
 
-							// Deactivate section.
-								$section.addClass('inactive');
+          // Deactivate section.
+          $section.addClass('inactive');
 
-						},
-						enter: function() {
+        },
+        enter: function() {
 
-							// Activate section.
-								$section.removeClass('inactive');
+          // Activate section.
+          $section.removeClass('inactive');
 
-							// No locked links? Deactivate all links and activate this section's one.
-								if ($nav_a.filter('.active-locked').length == 0) {
+          // No locked links? Deactivate all links and activate this section's one.
+          if ($nav_a.filter('.active-locked').length == 0) {
 
-									$nav_a.removeClass('active');
-									$this.addClass('active');
+            $nav_a.removeClass('active');
+            $this.addClass('active');
 
-								}
+          }
 
-							// Otherwise, if this section's link is the one that's locked, unlock it.
-								else if ($this.hasClass('active-locked'))
-									$this.removeClass('active-locked');
+          // Otherwise, if this section's link is the one that's locked, unlock it.
+          else if ($this.hasClass('active-locked'))
+            $this.removeClass('active-locked');
 
-						}
-					});
+        }
+      });
 
-			});
+    });
 
-	// Scrolly.
-		$('.scrolly').scrolly();
+  // Scrolly.
+  $('.scrolly').scrolly();
 
-	// Header (narrower + mobile).
+  // Header (narrower + mobile).
 
-		// Toggle.
-			$(
-				'<div id="headerToggle">' +
+  // Toggle.
+  $(
+    '<div id="headerToggle">' +
 					'<a href="#header" class="toggle"></a>' +
 				'</div>'
-			)
-				.appendTo($body);
+  )
+    .appendTo($body);
 
-		// Header.
-			$('#header')
-				.panel({
-					delay: 500,
-					hideOnClick: true,
-					hideOnSwipe: true,
-					resetScroll: true,
-					resetForms: true,
-					side: 'left',
-					target: $body,
-					visibleClass: 'header-visible'
-				});
+  // Header.
+  $('#header')
+    .panel({
+      delay: 500,
+      hideOnClick: true,
+      hideOnSwipe: true,
+      resetScroll: true,
+      resetForms: true,
+      side: 'left',
+      target: $body,
+      visibleClass: 'header-visible'
+    });
 
 })(jQuery);
