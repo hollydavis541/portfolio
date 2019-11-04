5161540006e66c7611f3fc93d5b880a17175a8e2
Holly Davis <hollydavis541@gmail.com>
2019-11-04 15:41:19 -0800

    added cors

--- server.js
+++ server.js
@@ -1,13 +1,22 @@
 'use strict';
 
+// Define packages needed
+// Require packages
 require('dotenv').config();
 const express = require('express');
 const app = express();
+const cors = require('cors');
 
+// Define a port to listen to
 const PORT = process.env.PORT || 3000;
 
+
+app.use(cors());
+
+// Define default routes and error - .static is for static items (see documentation)
 app.use(express.static('./public'));
 
+// Define functional routes
 app.get('/hello', (request, response) => {
   response.status(200).send('Hello');
 });
@@ -23,6 +32,8 @@ app.get('/data', (request, response) => {
   response.status(200).json(airplanes);
 });
 
+
 app.use('*', (request, response) => response.send('Sorry, that route does not exist.'))
 
+// Define listen port and actually listen
 app.listen(PORT,() => console.log(`Listening on port ${PORT}`));
